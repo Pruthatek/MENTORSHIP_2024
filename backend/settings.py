c374837c5a78f093bef826ba65c7628c12891448
wjoe01090 <drsuthar781@gmail.com>
2024-04-06 08:22:45 +0530

    new database updated

--- backend/settings.py
+++ backend/settings.py
@@ -33,8 +33,7 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "http://localhost:5173",
-    "https://seahorse-app-67utq.ondigitalocean.app"
+    "http://localhost:5173"
 ]
 
 INSTALLED_APPS = [
